036871f98286f83bd10ebd0bd6e4c0ef7bf9f99c
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-09-03 18:45:30 -0400

    updated variables

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -2,8 +2,4 @@ var randomResult;
 var losses;
 var win;
 
-// for (var i =0; i < 4; i++){
-//     var crystal = $("<div>");
-//         crystal.attr("class","crystal");
-//         $(".crystals").append(crystal);
-}
+
