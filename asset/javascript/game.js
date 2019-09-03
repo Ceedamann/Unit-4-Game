5c5b2d3d4fbdb2a2bd593deb04531e03d154a692
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-09-03 18:41:31 -0400

    declared variables

---
+++ asset/javascript/game.js
@@ -0,0 +1,9 @@
+var randomResult;
+var losses;
+var win;
+
+// for (var i =0; i < 4; i++){
+//     var crystal = $("<div>");
+//         crystal.attr("class","crystal");
+//         $(".crystals").append(crystal);
+}
