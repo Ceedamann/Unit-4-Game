24c508dd9494a36ca20de23d6a68bb69175276c7
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-09-03 21:13:36 -0400

    added created random guess,for loop to create divs and generate numbers for crystals, created div and assign random value

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -1,5 +1,23 @@
-var randomResult;
-var losses;
+var randomNumber;
+var losses
 var win;
+// creation of random guess//
+randomNumber = Math.floor(Math.random()*90)+30;
+console.log(randomNumber)
 
+// for loop to create the 4 div need 
+for (var i = 0; i < 4; i++){
 
+// random number generated for crytals
+    var random = Math.floor(Math.random() *11 ) +1;
+
+    console.log(random);
+
+//creation of div and assigning random value
+    var crystal = $("<div>");
+    crystal.attr({
+        "class": "crystal",
+        "data": random
+    })
+$(".crystals").append(crystal);
+}
